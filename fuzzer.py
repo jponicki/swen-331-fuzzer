b1c364feba7bd289d16c3a34ad1c0cc6a2ed0a04
jponicki <jxp3224@rit.edu>
2019-02-11 00:19:55 -0500

    Arguments working?

--- fuzzer.py
+++ fuzzer.py
@@ -1,1 +1,18 @@
-print('Hello World')
+import sys  # For system arguments
+import requests  # requests HTTP library
+
+if len(sys.argv) < 4:
+    print("incorrect number of arguments")
+
+else:
+    action = sys.argv[1]
+    url = sys.argv[2]
+
+    if action == "discover" or action == "test":
+        page = None
+        session = None
+        print('Action: ' + action)
+        print('URL' + url)
+
+    else:
+        print("invalid action")
