708b51d269268f373a234a284201906ee66b3847
jponicki <jxp3224@rit.edu>
2019-02-11 01:55:09 -0500

    Those nachos had... cumin... or something. WACK

--- fuzzer.py
+++ fuzzer.py
@@ -22,7 +22,7 @@ else:
             if custom_auth == 'dvwa':
                 print('here')
                 browser = mechanicalsoup.StatefulBrowser()
-                browser.open(url + "/" + 'dvwa')
+                browser.open(url) # + "/" + 'dvwa')
                 browser.get_current_page()
                 browser.select_form()
                 browser["username"] = "admin"
