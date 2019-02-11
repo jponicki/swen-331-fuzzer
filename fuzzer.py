69194876d0abee5bb50f2b0cf5edbf6f27aa8357
jponicki <jxp3224@rit.edu>
2019-02-11 00:53:05 -0500

    yuml sounds like yummy, so you'd think it would be fun

--- fuzzer.py
+++ fuzzer.py
@@ -1,5 +1,6 @@
 import sys  # For system arguments
 import requests  # requests HTTP library
+import mechanicalsoup
 
 if len(sys.argv) < 4:
     print("incorrect number of arguments")
@@ -7,12 +8,15 @@ if len(sys.argv) < 4:
 else:
     action = sys.argv[1]
     url = sys.argv[2]
-
+    option = sys.argv[3]
     if action == "discover" or action == "test":
         page = None
         session = None
         print('Action: ' + action)
         print('URL' + url)
+        print('Option' + option)
+
+
 
     else:
         print("invalid action")
